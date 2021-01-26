24c82d4436fe6a0868072f63f395c40d506484aa
jaysurplus@workstation <jaysurplus@workstation.private>
2021-01-26 15:11:20 +0800

    Init files.

---
+++ nooploop_uwb.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+
+import threading
+import serial
+import json
+from queue import Queue
+
+
+class Nooploop_UWB_AOA(object):
+    """nooploop uwb aoa parser.
+    """
+
+    def __init__(self, config_path=None):
+        """
+        Input:
+            config_path: str
+        """
+        def __searialthread():
+            while self.serthread_alive:
+                if self.serial.in_waiting:
+                    data = self.serial.read(self.serial.in_waiting)
+                    self.binbuffer.extend(data)
+                    print(data)
+                else:
+                    pass
+
+                try:
+                    while True:
+            
+                        self.data_fifo.put(self.binbuffer.pop(0))
+                except:
+                    pass
+
+        if config_path:
+            with open(config_path) as f:
+                self.config = json.load(f)
+            
+            port = self.config['port']
+            baudrate = self.config['baudrate']
+            role = self.config['role']
+
+        
+        self.serial = serial.Serial(port, baudrate, timeout=None)
+        self.data_fifo = Queue()
+        
+        self.serthread_alive = True
+        self.binbuffer = []
+
+        self.serthread = threading.Thread(target=__searialthread)
+        self.serthread.start()
+
+    def get_data(self, timeout=None):
+        data = self.data_fifo.get(block=True, timeout=timeout)
+        return data
+
+    def terminate(self):
+        """Terminal UWB Instance
+        """
+        self.serthread_alive = False
+        self.serial.close()
---
+++ test.py
@@ -0,0 +1,11 @@
+import nooploop_uwb
+
+uwb = nooploop_uwb.Nooploop_UWB_AOA('config.json')
+
+count = 0
+while count < 1000:
+    data = uwb.get_data()
+    #print(count,  data)
+    count += 1
+
+uwb.terminate()
